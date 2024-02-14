ffe7d80a618a23c36d175af3529b75da3d5219ed
Raazia-26 <razi.raazia@gmail.com>
2024-02-14 02:22:49 +0100

    added javascript code with fake data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+function ingredientName(response) {
+  event.preventDefault();
+  let ingName = document.querySelector("#input-text");
 
+  alert(`Finding recipe including ${ingName.value}...`);
+  console.log(ingName.value);
+
+  let fullRecipe = document.querySelector(".recipe");
+  let recipeName = document.querySelector(".recipe_name");
+
+  recipeName.innerHTML = `${ingName.value} Salad`;
+  fullRecipe.innerHTML = `<p> Beetroot </br> 
+  Yogurt</br>
+  Salt </br> </p>`;
+}
+
+let submitBtn = document.querySelector("#submit_btn");
+submitBtn.addEventListener("click", ingredientName);
