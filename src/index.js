cf91b13e45294d7cda7ff1fd3af40d4d924391e0
Raazia-26 <razi.raazia@gmail.com>
2024-02-17 13:02:56 +0100

    Improved the User Experience

--- src/index.js
+++ src/index.js
@@ -1,35 +1,23 @@
 function showRecipe(response) {
   let fullRecipe = document.querySelector(".recipe");
-  console.log(response.data.answer);
   fullRecipe.innerHTML = response.data.answer;
 }
-
 function ingredientName(response) {
   event.preventDefault();
 
   let ingName = document.querySelector("#input-text");
-  console.log(ingName.value);
+
   let apiKey = "dfd604f6396ftb7o693bf0062e2334aa";
   let context =
-    "Please provide short and easy recipe with maximum 4 ingredients. Make ingredients and instructions display in <p>. Make ingredients and instructions bold. Please display recipe name in bigger font size ";
+    "Please provide short and easy recipe with maximum 4 ingredients. Make ingredients and separate each line with a </br>. Make 'ingredients' and 'instructions' <strong>. Sign the poem 'SheCodes AI' at the end of the instructions inside <strong> and purple font color ";
   let prompt = `please generate a 4 ingredient recipe for ${ingName.value} Salad`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  let loading = document.querySelector("#load");
-  loading.innerHTML = "Generating Recipe ... ";
-
-  new Typewriter("#load", {
-    strings: ["Generating Recipe ... "],
-    autoStart: true,
-    delay: 75,
-  });
-
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
-
+  let fullRecipe = document.querySelector(".recipe");
+  fullRecipe.classList.remove("hidden");
+  fullRecipe.innerHTML = `<div class=loading> ⌛ Generating a ${ingName.value} Recipe for you...</div>`;
   axios.get(apiUrl).then(showRecipe);
 }
 
-/*https://api.shecodes.io/ai/v1/generate?prompt=please generate a 4 ingredient recipe for chicken Salad&&key=dfd604f6396ftb7o693bf0062e2334aa */
 let recipeGenerator = document.querySelector("#recipe-generator");
 recipeGenerator.addEventListener("submit", ingredientName);
