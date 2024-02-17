96c7cf7d529d4f436c4e350361e854bcc86fca86
Raazia-26 <razi.raazia@gmail.com>
2024-02-17 11:59:20 +0100

    integrated the API and JS typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
-function ingredientName(response) {
-  event.preventDefault();
-  let ingName = document.querySelector("#input-text");
-
-  alert(`Finding recipe including ${ingName.value}...`);
-  console.log(ingName.value);
-
+function showRecipe(response) {
   let fullRecipe = document.querySelector(".recipe");
-  let recipeName = document.querySelector(".recipe_name");
-
-  recipeName.innerHTML = `${ingName.value} Salad`;
-  fullRecipe.innerHTML = `<p> Beetroot </br> 
-  Yogurt</br>
-  Salt </br> </p>`;
+  console.log(response.data.answer);
+  fullRecipe.innerHTML = response.data.answer;
 }
 
-let submitBtn = document.querySelector("#submit_btn");
-submitBtn.addEventListener("click", ingredientName);
+function ingredientName(response) {
+  event.preventDefault();
+
+  let ingName = document.querySelector("#input-text");
+  console.log(ingName.value);
+  let apiKey = "dfd604f6396ftb7o693bf0062e2334aa";
+  let context =
+    "Please provide short and easy recipe with maximum 4 ingredients. Make ingredients and instructions display in <p>. Make ingredients and instructions bold. Please display recipe name in bigger font size ";
+  let prompt = `please generate a 4 ingredient recipe for ${ingName.value} Salad`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let loading = document.querySelector("#load");
+  loading.innerHTML = "Generating Recipe ... ";
+
+  new Typewriter("#load", {
+    strings: ["Generating Recipe ... "],
+    autoStart: true,
+    delay: 75,
+  });
+
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(showRecipe);
+}
+
+/*https://api.shecodes.io/ai/v1/generate?prompt=please generate a 4 ingredient recipe for chicken Salad&&key=dfd604f6396ftb7o693bf0062e2334aa */
+let recipeGenerator = document.querySelector("#recipe-generator");
+recipeGenerator.addEventListener("submit", ingredientName);
